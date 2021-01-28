3b849d2634130defe5b77fc99bb42c1f01b929ed
Krasimir <73705162+reveroJS@users.noreply.github.com>
2021-01-28 12:01:21 +0200

    add not found page

--- routes.js
+++ routes.js
@@ -4,8 +4,12 @@ const aboutController = require("./controllers/aboutController");
 
 const router = Router();
 
-router.use("/", productController);
 router.use("/about", aboutController);
+router.use("/", productController);
+
+router.get("*", (req, res) => {
+    res.render("404", { layout: false });
+});
 
 
 
